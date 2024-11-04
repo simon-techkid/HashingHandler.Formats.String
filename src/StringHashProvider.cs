06fb4983e9fa50b002723b596945eba734761672
simon-techkid <simon.techkid@outlook.com>
2024-11-03 16:42:01 -0800

    v1.3.0

---
+++ src/StringHashProvider.cs
@@ -0,0 +1,10 @@
+﻿// HashingHandler by Simon Field
+
+using System.Text;
+
+namespace HashingHandler.Formats.String;
+
+public class StringHashProvider(Encoding? encoding = null) : StringHashProviderBase<string>(encoding)
+{
+    protected override string ConvertToString(string data) => data;
+}
---
+++ src/StringHashVerifier.cs
@@ -0,0 +1,8 @@
+﻿// HashingHandler by Simon Field
+
+namespace HashingHandler.Formats.String;
+
+public class StringHashVerifier : HashVerifierBase<string>
+{
+    protected override IHashingProvider<string> HashProvider => new StringHashProvider();
+}
